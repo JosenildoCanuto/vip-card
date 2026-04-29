e8dd88a5f40ca5b34c22099ab4505171424e2bad
JosenildoCanuto <josenildo.acanuto@gmail.com>
2026-04-29 17:09:20 -0300

    feat: add page home

--- vip-card/app/page.tsx
+++ vip-card/app/page.tsx
@@ -3,61 +3,46 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+      <main className="flex flex-1 gap-4 w-full max-w-3xl flex-col items-center py-24 bg-zinc-50 dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
+          <h1 className="max-w-full text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Acesse o Aplicativo que deseja e aproveite os benefícios exclusivos
+            para membros do clube de vantagens Vip Card.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="grid grid-cols-2 gap-4 text-base font-medium">
+          <div className="flex flex-col gap-2 items-center">
+            <a
+              className="relative flex h-56 w-56 items-center justify-center rounded-2xl gap-2 bg-zinc-900 px-5 transition-colors hover:opacity-70 dark:hover:bg-[#ccc]"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                fill
+                src="/assets/vipcard.png"
+                alt="Vip Card"
+                className="object-cover rounded-2xl"
+              />
+            </a>
+            <p className="text-foreground">Vip Card</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <a
+              className="relative flex h-56 w-56 items-center justify-center rounded-2xl gap-2 bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                fill
+                src="/assets/seuclube.png"
+                alt="Seu Clube"
+                className="object-cover rounded-2xl transition-colors hover:opacity-50"
+              />
+            </a>
+            <p className="text-foreground">Seu Clube</p>
+          </div>
         </div>
       </main>
     </div>
