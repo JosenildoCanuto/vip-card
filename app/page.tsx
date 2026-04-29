83d72d8f67f4a58db321d2f321e8e9191b617c0e
JosenildoCanuto <josenildo.acanuto@gmail.com>
2026-04-29 17:52:14 -0300

    feat: add new link

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const apps = [
     label: "Vip Card",
   },
   {
-    href: "intent://br.com.servdonto.associado#Intent;scheme=android-app;end",
+    href: "intent://com.eurobem.app#Intent;scheme=android-app;end",
     src: "/assets/seuclube.png",
     alt: "Seu Clube",
     label: "Seu Clube",
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 gap-4 w-full max-w-3xl flex-col items-center py-24 bg-zinc-50 dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
+        <div className="flex flex-col px-4 items-center text-center sm:items-start sm:text-left">
           <h1 className="max-w-full text-xl sm:text-3xl font-semibold sm:leading-10 tracking-tight text-black dark:text-zinc-50">
             Acesse o Aplicativo que deseja e aproveite os benefícios exclusivos
             para membros do clube de vantagens Vip Card.
